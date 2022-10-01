12967892ad186c550134fa0bbce3120bcf6eda29
Yash <yashviradiyah7@gmail.com>
2022-10-01 15:51:35 +0530

    05-Menu completed

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState, useEffect } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
 
 function App() {
-  return <h2>menu project setup</h2>;
+  const [menu, setMenu] = useState(items);
+  const [categories, setCategories] = useState(
+    menu
+      .map((item) => item.category)
+      .filter((value, index, self) => self.indexOf(value) === index)
+      .concat(["All"])
+      .sort()
+  );
+
+  const filterItems = (cat) => {
+    const newItems =
+      cat === "All" ? items : items.filter((item) => item.category === cat);
+    setMenu(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories filterItems={filterItems} categories={categories} />
+        <Menu items={menu} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
--- 05-menu/setup/src/Categories.js
+++ 05-menu/setup/src/Categories.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ filterItems, categories }) => {
+  return (
+    <div className="btn-container">
+      {categories.map((category, index) => (
+        <button
+          type="button"
+          className="filter-btn"
+          key={index}
+          onClick={() => filterItems(category)}
+        >
+          {category}
+        </button>
+      ))}
+      {/* <button className="filter-btn" onClick={() => filterItems("ass")}>
+        yash
+      </button> */}
+    </div>
+  );
 };
 
 export default Categories;
